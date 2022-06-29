7a13b94e3fd704940ff75120c03a45d81b7aae66
adens <asepindrakurniawan@gmail.com>
2022-06-29 14:01:09 +0700

    header

--- app/chat/ChatView.js
+++ app/chat/ChatView.js
@@ -46,11 +46,7 @@ class ChatWindow extends PureComponent {
     this.leftHeight = new Animated.Value(0);
     this.paddingHeight = new Animated.Value(0);
     this.emojiHeight = new Animated.Value(0);
-    this.HeaderHeight = this.isIphoneX
-      ? iphoneXHeaderPadding + this.iosHeaderHeight
-      : Platform.OS === "android"
-      ? androidHeaderHeight
-      : this.iosHeaderHeight;
+    // this.HeaderHeight = this.isIphoneX ? iphoneXHeaderPadding + this.iosHeaderHeight : Platform.OS === 'android' ? androidHeaderHeight : this.iosHeaderHeight
     this.HeaderHeight = headerHeight;
     this.listHeight = height - this.HeaderHeight;
     this.isInverted = false;
